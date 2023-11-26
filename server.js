f44f6779955d5b4d5b598d765ab320a6aa13b673
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-26 21:24:32 +0100

    changes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const path = require('path');
 const cors = require('cors');
 const connectToDB = require('./db');
-const helmet = require('helmet');
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose')
+const helmet = require('helmet');
 
 // import routes
 const adsRoutes = require('./routes/ads.routes');
@@ -15,47 +15,47 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 app.use(helmet());
 
+const server = app.listen(process.env.PORT || 3030, () => {
+	console.log('Server is running on port: 3030');
+});
+
+// connect to DB
+connectToDB()
+
 // middleware for diferent ports client and server
 if(process.env.NODE_ENV !== 'production') {
   app.use(
     cors({
-      origin: ['http://localhost:3000'],
+      origin: ['http://localhost:3000', 'http://localhost:3030'],
       credentials: true,
     })
   );
 }
 
-const server = app.listen(process.env.PORT || 3030, () => {
-  console.log('Server is running on port: 3030');
-});
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-// connect to DB
-connectToDB()
-
-app.use(express.urlencoded({ extended: true }));   // x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));   // x-www-form-urlencoded
 app.use(express.json());    // form-data JSON format
 
-const sessionSecret = process.env.DB_SESSIONSECRET
 app.use(session({
-	secret: sessionSecret,
+	secret: process.env.DB_SESSIONSECRET,
 	store: MongoStore.create(mongoose.connection),
 	resave: false,
 	saveUninitialized: false,
 	cookie: {
 		secure: process.env.NODE_ENV == 'production',
 	},
+	unset: 'destroy',
 }));
 
 // add routes
 app.use('/api', adsRoutes); // add ads routes to server
 app.use('/api/auth', authRoutes); // add auth routes to server
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use((req, res) => {
-	res.status(404).json({ message: 'Not found...' });
+	res.status(404).json({ message: '404 Not found...' });
 });
 
 module.exports = server;
