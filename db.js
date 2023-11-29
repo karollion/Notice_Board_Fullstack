d08002246a22181cae247c969c6ea6c9d59d610d
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-11-29 22:10:45 +0100

    add user secret

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const connectToDB = () => {
   // connects our backend code with the database
 	const password = process.env.DB_PASSWORD
+	const user = process.env.DB_USER
   const NODE_ENV = process.env.NODE_ENV;
   let dbUri = '';
 
-  if(NODE_ENV === 'production') dbUri = `mongodb+srv://karollion:${password}@cluster0.pbd1wk2.mongodb.net/NoticeBoardDB?retryWrites=true&w=majority`;
+  if(NODE_ENV === 'production') dbUri = `mongodb+srv://${user}:${password}@cluster0.pbd1wk2.mongodb.net/NoticeBoardDB?retryWrites=true&w=majority`;
   else if(NODE_ENV === 'test') dbUri = 'mongodb://0.0.0.0:27017/NoticeBoardDBtest';
   else dbUri = 'mongodb://0.0.0.0:27017/NoticeBoardDB';
 
